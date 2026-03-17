44681d69ce80fc46406e58c116c36bcaf3763988
Come Bertrand <come.bertrand@protonmail.com>
2026-03-17 14:44:34 +0100

    Nix flake: install shell completions and man page

--- flake.nix
+++ flake.nix
@@ -12,6 +12,13 @@
           version = "0.2.0";
           src = ./.;
           cargoLock.lockFile = ./Cargo.lock;
+          nativeBuildInputs = [ pkgs.installShellFiles ];
+          postInstall = ''
+            installShellCompletion --bash target/*/build/zinc-cli-*/out/completions/zinc.bash
+            installShellCompletion --zsh --name _zinc target/*/build/zinc-cli-*/out/completions/_zinc
+            installShellCompletion --fish target/*/build/zinc-cli-*/out/completions/zinc.fish
+            installManPage target/*/build/zinc-cli-*/out/man/zinc.1
+          '';
         };
       }
     );
